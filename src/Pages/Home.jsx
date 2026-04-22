bd42b5d54dab6e67cc2e4b28f04f55d14836a58d
Mahdi Hasan <mahdihasanwebsite@gmail.com>
2026-04-22 22:20:31 +0600

    mehedi color😁

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-   <h1 className='bg-blue-600 text-red-500'>Ecobajar</h1>
+   <h1 className='bg-mehedi text-red-500'>Ecobajar</h1>
     </>
   )
 }
